3e7a11730238eea7ef274cfba7ca0bdd1da413aa
pipocaz <sirpedrotavares@gmail.com>
2015-02-07 20:49:05 +0000

    Version 2.0

--- b0t.rb
+++ b0t.rb
@@ -9,14 +9,14 @@ while 1
 
 
   begin
-    # obj.loading_configs
-    # break if obj.logout
-    #
-    # obj.hero_adventures
-    # sleep(2)
-    #
-    # obj.build_field
-    # sleep(2)
+    obj.loading_configs
+    break if obj.logout
+
+    obj.hero_adventures
+    sleep(2)
+
+    obj.build_field
+    sleep(2)
 
 
     if obj.configs[:farming]
@@ -33,7 +33,7 @@ while 1
 
     obj.back_to_home
     suspend=[*900..1600].sample
-    #suspend=20
+
     obj.log('debug', "Suspend b0t for #{suspend} seconds.")
     sleep(suspend)
 
